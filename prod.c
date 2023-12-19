1797b4f85161ec55b4512b31f079681176a92654
Joymangol.chingangbam <147248110+Joymangolch@users.noreply.github.com>
2023-12-19 12:34:40 +0530

    Updating prod.c

--- prod.c
+++ prod.c
@@ -5,5 +5,5 @@ int main()
 	printf("\n enter the value of num1 & num2");
 	scanf("\n %d %d", &num1, &num2);
 	prod =  num1*num2;
-	printf("the prod is :%d", prod);
+	printf("the product is :%d", prod);
 }
