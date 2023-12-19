122205184137f42a9ccf1359b1a86507cfb9f2ef
Joymangolch <147248110+Joymangolch@users.noreply.github.com>
2023-12-19 12:28:13 +0530

    updated

---
+++ prod.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,prod;
+	printf("\n enter the value of num1 & num2");
+	scanf("\n %d %d", &num1, &num2);
+	prod =  num1*num2;
+	printf("the prod is :%d", prod);
+}
